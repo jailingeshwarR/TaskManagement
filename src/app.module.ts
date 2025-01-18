8f4cb5fe53244092540eb991a8b13db6ae779250
JailingeshwarR <jailingeshwar.jr@gmail.com>
2025-01-18 12:29:23 +0530

    add schema changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, TaskModule],
+  imports: [AuthModule, TaskModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
